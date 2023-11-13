c1ec2d0cc6378bdd2d8a74ab283da028630d2960
nikthewanted <77376892+nikthewanted@users.noreply.github.com>
2023-11-13 20:16:32 +0530

    hhh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import react,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [long,setLong]=useState()
+  const [lati,setLati]=useState()
+  
+  const outer = {
+    textAlign:'center',
+    width:'80%',
+    margin:'auto',
+    display:'flex',
+    flexDirection:'column',
+    fontSize:"1.325rem",
+    justifyContent:'center',
+    padding:'20px',
+    backgroundColor:'red'
+
+  }
+  const inner={
+    height:'100px',
+    width:"50%",
+    margin:"auto",
+    display:'flex',
+    flexDirection:"column",
+    paddingTop:'10px',
+    // fontSize:'32px'
+  }
+  
+  const custompadd={
+    padding:"10px", 
+    fontSize:'20px'
+  }
+  const custButton ={
+    padding:"10px",
+    width:'50%',
+    margin:'auto',
+    fontSize:"20px"
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={outer}>
+    <h3>This is Weather React app</h3>
+    <div style={inner}>
+      <input style={custompadd} placeholder="Latitude" type="text" name='lat' 
+      onChange={e => setLati(e.target.value)}
+      />
+    </div>
+
+    <div style={inner}>
+      <input style={custompadd} placeholder="Langitude" type="text" name='lomg'
+      onChange={e => setLong(e.target.value)}
+      />
+    </div>
+
+    <button style={custButton}
+    
+    >Click Me</button>
+
+
+
     </div>
   );
 }
