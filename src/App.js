e6ca670164ade2c571fa7da004648ec0fb1cd076
nikthewanted <77376892+nikthewanted@users.noreply.github.com>
2023-11-16 01:10:16 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import react,{useState} from 'react';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [long,setLong]=useState()
-  const [lati,setLati]=useState()
+  const [city,setCity]=useState("")
+  const [selectCity,setSelectCity]=useState("")
+
+  
+
+  const getData = async() => {
+    const url = `https://the-weather-api.p.rapidapi.com/api/weather/${selectCity}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '06ba227d34mshf7b8ed3ef468cacp183958jsne2561aa23ed9',
+        'X-RapidAPI-Host': 'the-weather-api.p.rapidapi.com'
+      }
+    };
+  
+  
+    const response = await fetch(url, options);
+    const result = await response.json();
+    setCity(result.data)
+    console.log(city)
+    
+  }
+
+ 
   
   const outer = {
     textAlign:'center',
@@ -38,26 +60,35 @@ function App() {
     fontSize:"20px"
   }
 
+  
+
 
   return (
     <div style={outer}>
     <h3>This is Weather React app</h3>
     <div style={inner}>
-      <input style={custompadd} placeholder="Latitude" type="text" name='lat' 
-      onChange={e => setLati(e.target.value)}
+      <input style={custompadd} placeholder="City Name" type="text"
+      value={selectCity} 
+      onChange={e => setSelectCity(e.target.value)}
       />
     </div>
 
-    <div style={inner}>
-      <input style={custompadd} placeholder="Langitude" type="text" name='lomg'
-      onChange={e => setLong(e.target.value)}
-      />
-    </div>
+    <button style={custButton} onClick={getData}>Click Me</button>
 
-    <button style={custButton}
+    {
+      city ? <>
+        <h1>{city.city}</h1>
+        <h3>Temperature {city.temp} </h3>
+        <h3>Current waether {city.current_weather}</h3>
+        <h3>Wind {city.wind}</h3>
+        <h3>Humidity{city.humidity}</h3>
+      </>
+            
+
+      :null
+
+    }
     
-    >Click Me</button>
-
 
 
     </div>
